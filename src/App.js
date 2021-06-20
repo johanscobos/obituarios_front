504eefb7916cf066024707b0a94953ffb7ae4ffc
Albert Cobos <albertcobos@gmail.com>
2021-06-20 13:10:16 -0500

    primeros ajustes hoja de estilos

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Login from './components/Login';
 import CreateUser from './components/createUser';
 import CrudUser from './components/CrudUser';
+import Header from './components/Header'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   
   return (
-    <React.Fragment>
+    
+      <React.Fragment>
       <Router>
         <Switch>
           <Route path="/" exact render = {props=>(<Login{...props}/>)}></Route>
-          <Route path="/create-user" exact render = {props=>(<CreateUser{...props}/>)}></Route>
-          <Route path="/crudUser" exact render = {props=>(<CrudUser{...props}/>)}></Route>
+          {
+            <div className="grid-container">
+              <div className="container"><Header /> </div>
+              <div>
+              <Route path="/create-user" exact render = {props=>(<CreateUser{...props}/>)}></Route>
+              <Route path="/crudUser" exact render = {props=>(<CrudUser{...props}/>)}></Route>
+              </div>
+            </div>
+
+          }
+          
         </Switch>
       </Router>
     </React.Fragment>
+   
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import {UrlShowUsr} from    '../services/apirest';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEdit,faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap'
+class Header extends React.Component{ 
+
+    
+    
+render(){
+    return(
+        <div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        </div>
+        
+    );
+}
+}
+
+
+export default Header;
