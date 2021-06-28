548bec22b8a6880310069269c0e7f5652078dc15
johan sebastian cobos <johanscobos@gmail.com>
2021-06-27 19:02:49 -0500

    modificacion archivo app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import './assets/css/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from './components/Login';
 import CreateUser from './components/createUser';
-import CrudUser from './components/CrudUser';
+import crudUser from './components/CrudUser';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Routes from './routes/routes';
+import Routes from './routes/routes'
 function App() {
   
   return (
-    <React.Fragment>
-        <Routes/>
-    </React.Fragment>
+    <Routes/>
   );
 }
 
