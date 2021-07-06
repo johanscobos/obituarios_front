fd65eeb743b6805a3cd22671fd67984208a07322
Albert Cobos <albertcobos@gmail.com>
2021-07-06 03:56:41 -0500

    Diseño obituatrios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,12 @@ import axios from 'axios';
 import logoOfrenda from '../assets/img/logo-la-ofrenda.png';
 import iconoLinea from '../assets/img/icono-linea-gratuita.png';
 import Obituarios from '../components/Obituarios';
-import {UrlShowObit} from    '../services/apirest';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faSearch,faUser} from '@fortawesome/free-solid-svg-icons'
+//import {UrlShowObit} from    '../services/apirest';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch,faUser,faShoppingCart,faMobileAlt} from '@fortawesome/free-solid-svg-icons';
 import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap'
 class Home extends React.Component{ 
-    componentDidMount(){
+    /*componentDidMount(){
         this.peticionGet();
         }
     peticionGet=()=>{
@@ -16,26 +16,56 @@ class Home extends React.Component{
          await this.setState({obituarios: response.data[0]});
         })
         }
+*/
     
 render(){
     return(
         <>
         <header className="header">
-            <div className="header_top"></div>
+            <div className="header_top">
+                <div className="container">
+                
+                <ul className="header_top_items">
+                    <li><FontAwesomeIcon icon={faUser} className="header_top_item"/></li>
+                    <li><FontAwesomeIcon icon={faUser} className="header_top_item"/></li>
+                    <li><FontAwesomeIcon icon={faUser} className="header_top_item"/></li>
+                    <li><FontAwesomeIcon icon={faUser} className="header_top_item"/></li>
+                    <li><FontAwesomeIcon icon={faUser} className="header_top_item"/></li>     
+                               
+                </ul>
+                </div>
+            </div>
             <div className="header_down container">
                 <div><img src={logoOfrenda} alt="logo La Ofrenda" /></div>
                 <div className="header_down_icono-linea">
                     <img src={iconoLinea} alt="icono linea gratuita" />
                     <h2>LÍNEA GRATUITA NACIONAL: <br />01 8000 18 08 28</h2>
-                    </div>
-                <div><button type="button" class="btn btn-primary btn-sesion">INICIAR SESION</button></div>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-primary btn-paga-aqui"><FontAwesomeIcon icon={faShoppingCart} /><span className="btn-text">PAGA AQUÍ</span></button>
+                    <button type="button" class="btn btn-primary btn-descarga-app"><FontAwesomeIcon icon={faMobileAlt} /><span className="btn-text">DESCARGA APP</span></button>
+                </div>
+                
             </div>
             
         </header>
-        <main className="container">
+        <div className="obituarios-titulo"> 
+            <div className="container">
+                <p>Home</p>
+                <h1>Obituarios</h1>
+            </div>   
+            <div className="obituarios-aux-titulo-overlay"></div>         
+        </div> 
+        <main className="container home-main">
            
             <Obituarios />
         </main>
+
+        <footer className="footer">
+            <div className="container text-center p-3 footer-text">
+            <h6>© 2021 La Ofrenda S.A. Todos los derechos reservados.<br /> Diseño y Desarrollo: Táctica S.A.S</h6>
+            </div>
+        </footer>
         
       </> 
     );
